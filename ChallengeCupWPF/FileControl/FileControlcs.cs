dd82cc87175b2ba3e376aa5ae7ca07d7fe210055
Daniel-Lee <706321263@qq.com>
2016-10-23 15:53:12 +0800

    添加项目文件。

---
+++ ChallengeCupWPF/FileControl/FileControlcs.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeCupWPF
+{
+    class FileControlcs
+    {
+        /// <summary>
+        /// read data form file
+        /// </summary>
+        /// <param name="filePath">file path</param>
+        /// <returns>data from file</returns>
+        public static Task<List<float>[]> ReadDataAsync(string filePath)
+        {
+#if DEBUG
+            Console.WriteLine("file is " + filePath);
+#endif
+            if (File.Exists(filePath))
+            {
+#if DEBUG
+                Console.WriteLine("file is valid");
+#endif
+                List<float>[] chanels;
+                return Task.Run(() =>
+                {
+                    // begin to read data form file
+                    using (StreamReader reader = new StreamReader(filePath))
+                    {
+                        // read chanels' number
+                        int chanelNumber = int.Parse(reader.ReadLine());
+#if DEBUG
+                        Console.WriteLine("chanel number is " + chanelNumber);
+#endif
+                        // initial chanels[]
+                        chanels = new List<float>[chanelNumber];
+                        for (int i = 0; i < chanels.Length; i++)
+                        {
+                            chanels[i] = new List<float>();
+                        }
+                        // read all data form file
+                        string text = reader.ReadToEnd();
+                        #region SpiltData
+                        // it costs less time than RegexMatch
+                        string[] data = text.Replace("\r\n", " ")
+                            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+#if DEBUG
+                        foreach (var d in data)
+                        {
+                            Console.WriteLine(d);
+                        }
+#endif
+                        // parse and add data to chanels
+                        for (int i = 0; i < data.Length; i++)
+                        {
+                            chanels[i % chanels.Length].Add(float.Parse(data[i]));
+                        }
+                        #endregion
+                    }
+                    return chanels;
+                });
+                // 文件不存在
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// write data to file
+        /// </summary>
+        /// <param name="data">data to write</param>
+        /// <param name="filePath">file path</param>
+        public static Task WriteData(List<float>[] data, string filePath)
+        {
+#if DEBUG
+            Console.WriteLine("write to file " + filePath);
+#endif
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                // write number of chanels to file
+                writer.WriteLine(data.Length);
+#if DEBUG
+                Console.WriteLine("chanel number is " + data.Length);
+#endif
+                // write data
+                for (int i = 0; i < data[0].Count; i++)
+                {
+                    foreach (var item in data)
+                    {
+                        writer.Write(item[i] + "\t");
+                    }
+                    writer.WriteLine();
+                }
+#if DEBUG
+                Console.WriteLine("write done");
+#endif
+            }
+            return null;
+        }
+    }
+}
---
+++ ChallengeCupWPF/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Research.DynamicDataDisplay;
+using Microsoft.Research.DynamicDataDisplay.DataSources;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ChallengeCupWPF
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableDataSource<Point>[] dataSource = new ObservableDataSource<Point>[8];
+        //private PerformanceCounter cpuPerformance = new PerformanceCounter();
+        private DispatcherTimer timer = new DispatcherTimer();
+        private List<float>[] dataList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AnimatedPlot(object sender, EventArgs e)
+        {
+            //cpuPerformance.CategoryName = "Processor";
+            //cpuPerformance.CounterName = "% Processor Time";
+            //cpuPerformance.InstanceName = "_Total";
+
+            //double x = i;
+            //double y = cpuPerformance.NextValue();
+
+            //Point point = new Point(x, y);
+            //dataSource.AppendAsync(base.Dispatcher, point);
+            //cpuUsageText.Text = String.Format("{0:0}%", y);
+            //i++;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < dataSource.Length; i++)
+            {
+                dataSource[i] = new ObservableDataSource<Point>();
+                plotter.AddLineGraph(dataSource[i], Colors.Green, 2, " ");
+            }
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += new EventHandler(AnimatedPlot);
+            timer.IsEnabled = true;
+            plotter.Viewport.FitToView();
+        }
+
+        /// <summary>
+        /// Open file and read data
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "txt file|*.txt";
+            if (openFile.ShowDialog() == true)
+            {
+#if DEBUG
+                Console.WriteLine("open file: " + openFile.FileName);
+#endif
+                // Read form file
+                dataList = await FileControlcs.ReadDataAsync(openFile.FileName);
+                // Add data to dataSource
+                for (int i = 0; i < dataList.Length; i++)
+                {
+                    for (int j = 0; j < dataList[i].Count; j++)
+                    {
+                        dataSource[i].AppendAsync(base.Dispatcher, new Point(j, dataList[i][j]));
+                    }
+                }
+            }
+        }
+
+        private async void SaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            await FileControlcs.WriteData(dataList, "c:\\");
+        }
+    }
+}
