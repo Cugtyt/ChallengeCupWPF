3960a0f740bab5e72faff169a359c28877ffc684
Daniel-Lee <706321263@qq.com>
2017-04-19 22:21:16 +0800

    Add udp sent example.

--- ChallengeCupV2/UDP/UDPRead.cs
+++ ChallengeCupV2/UDP/UDPRead.cs
@@ -111,5 +111,133 @@ namespace ChallengeCupV2.UDP
             udpClient.Close();
             ((IDisposable)udpClient).Dispose();
         }
+//        class Program
+//        {
+//            static int count;
+//            static void Main(string[] args)
+//            {
+//                UdpClient udpClient = new UdpClient(100);
+//                IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 60);
+//                double a1 = 0.01;
+//                double a2 = 1545.5;
+//                double a3 = 1545.6;
+//                Random rd = new Random();
+//                int count = 0;
+
+//                #region Test Case 1
+//                while (true)
+//                {
+//                    double ran = rd.NextDouble() / 100;
+//                    double sina1 = Math.Sin(a1) / 100;
+//                    Byte[] message = Encoding.ASCII.GetBytes(string.Format("2 1 1 0 {1} {2} {1} {2}", sina1 + ran, a2 + ran, a3 + ran));
+//                    udpClient.Send(message, message.Length, endPoint);
+//                    //Thread.Sleep(1);
+//                    Console.Write(++count >= 6000 ? DateTime.Now.ToString() + $"\t{count}\n" : "");
+//                    count %= 6000;
+//                    a1 += 0.08;
+//                }
+//                //UdpClient udpClient = new UdpClient(60);
+//                //IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 100);
+//                //count = 0;
+//                //while (true)
+//                //{
+//                //    string tempRecv = Encoding.ASCII.GetString(
+//                //        udpClient.Receive(ref endPoint));
+//                //    if (tempRecv != null)
+//                //    {
+//                //        count++;
+//                //        if (count > 500)
+//                //        {
+//                //            Console.WriteLine(count);
+//                //            count = 0;
+//                //        }
+//                //    }
+//                //}
+//                #endregion
+//                #region Test Case 2
+//                //var data = GetDataFrom(ReadDataFromFile(@"C:\debug\FBG解调系统数据文件\数据文件\2017-04-05\0_12_70\PeaksAll 2017-04-05 22-43-34 14-Wednesday.txt"));
+//                //int index = 0;
+//                //while (true)
+//                //{
+//                //    Byte[] message = Encoding.ASCII.GetBytes(string.Format("0 1 0 0 {0}", data[0][index++]));
+//                //    index %= data[0].Count();
+//                //    udpClient.Send(message, message.Length, endPoint);
+//                //    Thread.Sleep(1);
+//                //    Console.Write(++count >= 6000 ? DateTime.Now.ToString() + $"\t{count}\n" : "");
+//                //    count %= 6000;
+//                //}
+//                #endregion
+//            }
+
+//            public static string[] ReadDataFromFile(string filePath)
+//            {
+//                if (filePath == null)
+//                {
+//#if DEBUG
+//                Console.WriteLine("FileUtils: ReadWaveData() -> Illegal input, argument can not be null.");
+//#endif
+//                    throw new ArgumentNullException("FileUtils: ReadWaveData()");
+//                }
+
+//                if (!System.IO.File.Exists(filePath))
+//                {
+//#if DEBUG
+//                Console.WriteLine("FileUtils: ReadWaveData() -> file is not valid");
+//#endif
+//                    throw new FileNotFoundException("FileUtils: ReadWaveData() -> file " + filePath + "is not valid.");
+//                }
+//#if DEBUG
+//            Console.WriteLine("FileUtils: ReadWaveData() -> file is valid");
+//#endif
+//                //List<double>[] dataList = new List<double>[4];
+//                //for (int i = 0; i < dataList.Length; i++)
+//                //{
+//                //    dataList[i] = new List<double>();
+//                //}
+//                //return Task.Run(() =>
+//                //{
+//                string text;
+//                // begin to read data form file
+//                using (StreamReader reader = new StreamReader(filePath))
+//                {
+//                    reader.ReadLine();
+//                    reader.ReadLine();
+//                    reader.ReadLine();
+//                    text = reader.ReadToEnd();
+//                }
+//                text = text.Substring(0, text.LastIndexOf('\n'));
+//                return text.Replace("\r", " ").Replace("\t", " ").Replace("\n", " ")
+//                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+//                //});
+//            }
+
+//            public static List<double>[] GetDataFrom(string[] data)
+//            {
+//                List<double>[] Data;
+//                int len;
+//                try
+//                {
+//                    len = int.Parse(data[1]) + int.Parse(data[2]) + int.Parse(data[3]) + int.Parse(data[4]);
+//                    Data = new List<double>[len];
+//                    for (int i = 0; i < Data.Length; i++)
+//                    {
+//                        Data[i] = new List<double>();
+//                    }
+//                }
+//                catch (Exception)
+//                {
+//                    throw new Exception("File formate is illegal.");
+//                }
+
+//                for (int i = 0; i < data.Length; i++)
+//                {
+//                    if (i % (6 + len) > 5)
+//                    {
+//                        Data[i % (6 + len) - 6].Add(double.Parse(data[i]));
+//                    }
+//                }
+//                return Data;
+//            }
+//        }
     }
 }
